61a9c9410ec30bead3828aa374039c276efc2f82
BogdanProkopenko1 <integer.args@gmail.com>
2020-11-15 13:25:30 +0500

    4.3.1. Оператор && [#405745]

--- src/ru/job4j/condition/Cinema.java
+++ src/ru/job4j/condition/Cinema.java
@@ -1,20 +1,18 @@
 package ru.job4j.condition;
 
 public class Cinema {
-    public static void access(int age) {
-        System.out.println("The age of the customer is : " + age);
-        if (age >= 18) {
-            System.out.println("Welcome to the cinema.");
-            System.out.println(" ");
+    public static void permission(boolean allowByParent, boolean hasMoney) {
+        if (allowByParent && hasMoney) {
+            System.out.println("I can go to the cinema.");
         } else {
-            System.out.println("It is not for you.");
-            System.out.println(" ");
+            System.out.println("I cant.");
         }
     }
+
     public static void main(String[] args) {
-        Cinema.access(11);
-        Cinema.access(58);
-        Cinema.access(87);
-        Cinema.access(15);
+        permission(true, true);
+        permission(false, true);
+        permission(true, false);
+        permission(false, false);
     }
 }
