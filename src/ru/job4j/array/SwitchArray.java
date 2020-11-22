cdef5a32bed66bd956288c51cb0b87b4a3272aad
BogdanProkopenko1 <integer.args@gmail.com>
2020-11-23 03:41:39 +0500

    6.2.1. Выход за границу массива. [#405777]

---
+++ src/ru/job4j/array/SwitchArray.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+public class SwitchArray {
+
+    public static int[] swapBorder(int[] array) {
+        int change0 = array[0];
+        int change1 = array[array.length - 1];
+        array[0] = change1;
+        array[array.length - 1] = change0;
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {1, 2, 3, 4, 5, 6};
+        int[] rsl = swapBorder(nums);
+        for (int index = 0; index < rsl.length; index++) {
+            System.out.println(rsl[index]);
+        }
+    }
+}
