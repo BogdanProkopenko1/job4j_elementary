8275d2fc8d244228d04d4348235e88c56e3c0f00
BogdanProkopenko1 <integer.args@gmail.com>
2020-11-22 15:10:51 +0500

    5.5. Простое число [#405852]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -5,7 +5,7 @@ public class CheckPrimeNumber {
     public static boolean check(int number) {
         boolean checkResult = number > 1;
         for (int i = 2; i < number; i++) {
-            if (number % 2 == 0) {
+            if (number % i == 0) {
                 checkResult = false;
                 break;
             }
