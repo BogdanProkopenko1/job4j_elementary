ef5e153f96c4a14e27e0a581e460d18e72e2f61b
BogdanProkopenko1 <integer.args@gmail.com>
2020-12-03 19:50:59 +0500

    1.7. Состояние объекта [#405958]

---
+++ src/ru/job4j/oop/Battery.java
@@ -0,0 +1,30 @@
+package ru.job4j.oop;
+
+public class Battery {
+
+    private int value;
+
+    public Battery(int size) {
+        this.value = size;
+    }
+
+    public void exchange(Battery another) {
+        this.value = this.value - another.value;
+    }
+
+    public static void main(String[] args) {
+        Battery charge = new Battery(100);
+        Battery discharge = new Battery(5);
+        System.out.println("before " + charge.value);
+        charge.exchange(discharge);
+        System.out.println("after " + charge.value);
+        charge.exchange(discharge);
+        System.out.println("after " + charge.value);
+        charge.exchange(discharge);
+        System.out.println("after " + charge.value);
+        charge.exchange(discharge);
+        System.out.println("after " + charge.value);
+        charge.exchange(discharge);
+        System.out.println("after " + charge.value);
+    }
+}
