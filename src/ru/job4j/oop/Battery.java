d97b3fa230aa2c9f06384f25a2034f26e1bc9b32
BogdanProkopenko1 <integer.args@gmail.com>
2020-12-05 19:02:37 +0500

    1.7. Состояние объекта [#405958] fix 2

--- src/ru/job4j/oop/Battery.java
+++ src/ru/job4j/oop/Battery.java
@@ -9,15 +9,15 @@ public class Battery {
     }
 
     public void exchange(Battery another) {
-        this.value = this.value - another.value;
-        another.value = 0;
+        another.value = another.value + this.value;
+        this.value = 0;
     }
 
     public static void main(String[] args) {
-        Battery charge = new Battery(100);
-        Battery discharge = new Battery(5);
-        System.out.println("before " + charge.value);
-        charge.exchange(discharge);
-        System.out.println("after " + charge.value);
+        Battery energy = new Battery(95);
+        Battery power = new Battery(5);
+        System.out.println("before " + energy.value);
+        power.exchange(energy);
+        System.out.println("after " + energy.value);
     }
 }
