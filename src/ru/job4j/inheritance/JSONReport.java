50c9c66244e9f5aa199ed32dfa14afe79d960582
BogdanProkopenko1 <integer.args@gmail.com>
2020-12-09 16:54:31 +0500

    5. Аннотация @Override [#405966]

--- src/ru/job4j/inheritance/JSONReport.java
+++ src/ru/job4j/inheritance/JSONReport.java
@@ -1,6 +1,12 @@
 package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
+
+    @Override
+    public String generate(String name, String body) {
+        return "name : " + name + System.lineSeparator() + "body : " + body;
+    }
+
     public static void main(String[] args) {
         JSONReport report = new JSONReport();
         String rsl = report.generate("error", "404");
