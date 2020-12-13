b64d67331a35a5ea85a28b73995b5b23864a9eeb
BogdanProkopenko1 <integer.args@gmail.com>
2020-12-13 16:43:11 +0500

    2. Scanner и чтение числа из консоли. [#405902] fix

--- src/ru/job4j/io/Matches.java
+++ src/ru/job4j/io/Matches.java
@@ -5,19 +5,32 @@ import java.util.Scanner;
 public class Matches {
 
     public static void main(String[] args) {
-        System.out.println("Game of eleven matches." + System.lineSeparator() + "Player number 1 is walking.");
+        System.out.println("Game of eleven matches." + System.lineSeparator());
         int sticks = 11;
+        int counter = 0;
+        Scanner in = new Scanner(System.in);
+        String player1 = in.nextLine();
+        String player2 = in.nextLine();
         while (sticks > 0) {
-            System.out.println("left is " + sticks + System.lineSeparator());
-            Scanner in = new Scanner(System.in);
+            System.out.println("left is " + sticks);
             int select = Integer.valueOf(in.nextLine());
+            if (counter % 2 == 0) {
+                System.out.println("Player " + player1 + " walked.");
+            } else {
+                System.out.println("Player " + player2 + " walked.");
+            }
             if (select > 0 && select < 4 && sticks - select >= 0) {
                 sticks -= select;
+                counter++;
             } else {
                 System.out.println("Choice is impossible. Try again.");
             }
             if (sticks == 0) {
-                System.out.println("This player is winner!");
+                if (counter % 2 == 0) {
+                    System.out.println("Player " + player1 + " is winner!");
+                } else {
+                    System.out.println("Player " + player2 + " is winner!");
+                }
             }
         }
     }
