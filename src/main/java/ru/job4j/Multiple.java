933e8ac819b4877087805259494f5e43865bd0b0
BogdanProkopenko1 <integer.args@gmail.com>
2021-04-06 18:38:44 +0500

    0.1. FileOutputStream. [#482516] 2

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -1,15 +1,14 @@
 package ru.job4j;
 
+import java.io.FileOutputStream;
+
 public class Multiple {
+
     public static void main(String[] args) {
-        System.out.println("1*1=1");
-        System.out.println("1*2=2");
-        System.out.println("1*3=3");
-        System.out.println("1*4=4");
-        System.out.println("1*5=5");
-        System.out.println("1*6=6");
-        System.out.println("1*7=7");
-        System.out.println("1*8=8");
-        System.out.println("1*9=9");
+        try (FileOutputStream file = new FileOutputStream("FILE.txt")) {
+            file.write(("1*1=1" + "1*2=2" + "1*3=3" + "1*4=4" + "1*5=5" + "1*6=6" + "1*7=7" + "1*8=8" + "1*9=9").getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
