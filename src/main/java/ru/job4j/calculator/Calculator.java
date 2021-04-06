e54c81b898e7ccfe1f1ace59f27d0001e802da46
BogdanProkopenko1 <integer.args@gmail.com>
2021-04-06 18:13:00 +0500

    0.1. FileOutputStream. [#482516]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,6 +1,9 @@
 package ru.job4j.calculator;
 
+import java.io.FileOutputStream;
+
 public class Calculator {
+
     public static void main(String[] args) {
         int one = 1;
         int two = 2;
@@ -13,9 +16,15 @@ public class Calculator {
         int fiveMinusTwo = five - two;
         int fourTimeTwo = four * two;
 
-        System.out.println(onePlusTwo);
-        System.out.println(sixDivTwo);
-        System.out.println(fiveMinusTwo);
-        System.out.println(fourTimeTwo);
+        try (FileOutputStream file = new FileOutputStream("example.txt")) {
+            file.write(
+                    (onePlusTwo + System.lineSeparator()
+                            + sixDivTwo + System.lineSeparator()
+                            + fiveMinusTwo + System.lineSeparator()
+                            + fourTimeTwo).getBytes()
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
